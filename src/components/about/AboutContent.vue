df7d6c9bb8e134e95e203d1cfda4259f2e8983b7
hiramatsu <hiramatsu3300@gmail.com>
2019-03-10 13:45:24 +0900

    full screen carousel build

--- src/components/about/AboutContent.vue
+++ src/components/about/AboutContent.vue
@@ -4,52 +4,16 @@
       <div class="main-header">
         <section class="main-header-text">head-line</section>
       </div>
-      <div class="main-content">
-        <section class="main-content-title">head-line</section>
-        <section class="main-content-body">body-line</section>
-      </div>
-      <div class="main-footer">
+      <transition name="mainCon" appear>
+        <div class="main-content">
+          <section class="main-content-title">head-line</section>
+          <section class="main-content-body">body-line</section>
+        </div>
+      </transition>  
+      <!-- <div class="main-footer">
         <section class="main-footer-logo">head-line</section>
         <section class="main-footer-menu">body-line</section>
         <section class="main-footer-police">head-line</section>
-      </div>
-      <!-- <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-              <div class="main-header">
-                <div class="main-header-wrape">
-                  <p>main header</p>
-                  <h1>Vue-cli3 Template About</h1>
-                </div>
-              </div>
-            </div>
-            <div class="col-12">
-              <div class="main-content">
-                <p>main content</p> 
-                <section class="main-section">
-                  <p>main section1</p> 
-                  <h1>Vue-cli3 Template Text</h1>
-                  <h1>h1:2.5rem</h1>
-                  <h2>h2:2.0rem</h2>
-                  <h3>h3:1.75rem</h3>
-                  <h4>h4:1.5rem</h4>
-                  <h5>h5:1.25rem</h5>
-                  <h6>h6:1.0rem</h6>
-                  <p>p:1.0rem</p>
-                </section>
-                <section class="main-section">
-                  <p>main section2</p> 
-                  <h1>Vue-cli3 Template Detail</h1>
-                </section>
-              </div>
-            </div>
-            <div class="col-12">
-              <div class="main-footer">
-                <p>main footer</p>
-                <h3>h-Works vue tenplate v0.1</h3>
-              </div>
-            </div>
-        </div>
       </div> -->
     </main>
   </div>
@@ -79,8 +43,9 @@ main{
 .main-header{
   width:100%;
   height: 25vh;
+  // height: calc( 100vh - #{ $header-height } ) ;//full header
   padding: 1rem 1rem;
-  border: 1px solid green;
+  // border: 1px solid green;
   //横スクロール処理
   width: auto;
   min-width: 100vw;
@@ -89,6 +54,8 @@ main{
   width:100%;
   // height: auto;
   height: 75vh;
+
+
   padding: 1rem 1rem;
   border: 1px solid lightblue;
   background-color: $main-contents-color;
--- src/components/works/WorksContent.vue
+++ src/components/works/WorksContent.vue
@@ -2,57 +2,18 @@
   <div class="worksContent">
     <main>
       <div class="main-header">
-        <section class="main-header-text">
-          head-line
-        </section>
+        <section class="main-header-text">head-line</section>
       </div>
-
       <transition name="mainCon" appear>
-        <div id="mainContent" class="main-content">
-        <!-- <div id="mainContent" class="main-content" 
-        v-bind:style="{ transform: 'translateY(' + mainPos + 'px )'  }"> -->
+        <div  class="main-content">
             <section class="main-content-title">head-line</section>
             <section class="main-content-body">body-line</section>
         </div>
       </transition>
-
       <!-- <div class="main-footer">
         <section class="main-footer-logo">head-line</section>
         <section class="main-footer-menu">body-line</section>
         <section class="main-footer-police">head-line</section>
-      </div> -->
-      <!-- <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-              <div class="main-header">
-                <div class="main-header-wrape">
-                  <p>main header</p>
-                  <h1>Vue-cli3 Template Works</h1>
-                </div>
-              </div>
-            </div>
-            <div class="col-12">
-              <section class="main-section">
-                <p>main section</p>
-                <h1>Vue-cli3 Template Detail</h1>
-                <ul>
-                  <li>
-                    <p>unit</p>
-                    <p>unit</p>
-                    <p>unit</p>
-                    <p>unit</p>
-                    <p>unit</p>
-                  </li>
-                </ul>
-              </section>
-            </div>
-            <div class="col-12">
-              <div class="main-footer">
-                <p>main footer</p>
-                <h3>h-works vue tenplate v0.1</h3>
-              </div>
-            </div>
-        </div>
       </div> -->
     </main>
   </div>
@@ -64,36 +25,36 @@ export default {
   props: {
     msg: String,
   },
-  data() {
-    return{
-      mainPos: -500,
-      innerHeight: 0,
-      mainContentPos: 0,
-    }
-  },
-  mounted: function () {
-    window.addEventListener('scroll', this.handleScroll);
-    this.init();
-  },
-  destroyed: function () {
-    window.removeEventListener('scroll', this.handleScroll);
-  },
-  methods: {
-    handleScroll() {
-      this.mainContentPos = document.getElementById('mainContent').getBoundingClientRect().top ;
-      this.innerHeight = window.innerHeight;
-      this.resize();
-    },
-    init(){
-      this.mainContentPos = document.getElementById('mainContent').getBoundingClientRect().top ;
-      this.innerHeight = window.innerHeight;
+  // data() {
+  //   return{
+      // mainPos: -500,
+      // innerHeight: 0,
+      // mainContentPos: 0,
+  //   }
+  // },
+  // mounted: function () {
+  //   window.addEventListener('scroll', this.handleScroll);
+  //   this.init();
+  // },
+  // destroyed: function () {
+  //   window.removeEventListener('scroll', this.handleScroll);
+  // },
+  // methods: {
+  //   handleScroll() {
+  //     this.mainContentPos = document.getElementById('mainContent').getBoundingClientRect().top ;
+  //     this.innerHeight = window.innerHeight;
+  //     this.resize();
+  //   },
+  //   init(){
+  //     this.mainContentPos = document.getElementById('mainContent').getBoundingClientRect().top ;
+  //     this.innerHeight = window.innerHeight;
      
-      this.resize();
-    },
-    resize(){
-       this.mainPos = (this.innerHeight - 300 -50 )  * -1  ;
-    }
-  },
+  //     this.resize();
+  //   },
+  //   resize(){
+  //      this.mainPos = (this.innerHeight - 300 -50 )  * -1  ;
+  //   }
+  // },
 }
 </script>
 
@@ -109,24 +70,26 @@ main{
   margin: 0;
   padding: 0;
 }
-
 .main-header{
   width:100%;
-  height: calc( 100vh - #{ $header-height } ) ;
+  height: 25vh;
+  // height: calc( 100vh - #{ $header-height } ) ;//full header
   padding: 1rem 1rem;
-  border: 1px solid green;
+  // border: 1px solid green;
   //横スクロール処理
   width: auto;
   min-width: 100vw;
 }
 .main-content{
-  position: absolute;
-  top: 30vh;
-  left: 0;
-  // z-index: 1;
-  width:100%;
-  height: 200vh;
+  // position: absolute;
+  // top: 30vh;
+  // left: 0;
+  // width:100%;
+  // height: 200vh;
+
+  height: 75vh;
   padding: 1rem 1rem;
+  
   background-color: $main-contents-color;
   color: $main-contents-text;
   //横スクロール処理
@@ -144,18 +107,5 @@ main{
   // width: auto;
   // min-width: 100vw;
 // }
-.mainCon-enter-active, .mainCon-leave-active {
-    // transition: all .5s 1s ease-out;
-     //transition: all .1s  .5s  cubic-bezier(0.320, 0.625, 0.580, 1.000);//sharp
-    //  transition: all .5s  1s  cubic-bezier(0.170, 0.935, 0.305, 1.000)//stylish
-    // transition: all 1s  .5s  cubic-bezier(0.330, 1.700, 0.510, 0.820);//bounds
-     transition: all 1s  .5s  cubic-bezier(0.545, 0.080, 0.520, 0.975);//soft
-}
-.mainCon-enter, .mainCon-leave-to {
-    // opacity: 1;
-    transform: translateY(2000px);
-    // position: absolute;
-    // top: 30vh;
-    // left: 0;
-}
+
 </style>
